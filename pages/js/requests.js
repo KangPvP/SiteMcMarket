71a88e7b14684755a2b6097bb3ee1d6d4a5f23d8
KangPvP <82943439+KangPvP@users.noreply.github.com>
2023-03-08 21:59:05 +0100

    InteractionBtn
    Co-authored-by: ArkaOnYtb <ArkaOnYtb@users.noreply.github.com>

---
+++ pages/js/requests.js
@@ -0,0 +1,33 @@
+const url = 'http://127.0.0.1:8080/'
+
+async function login(email, password) {
+
+    const formData = {"email": email, "password": password}
+    console.log(formData)
+
+    let request = await fetch(url + 'users/login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+    })
+    let reqjson = await request.json()
+    console.log(reqjson)
+    return reqjson
+}
+
+async function register(pseudo, email, password) {
+    let request = await fetch(url + 'users/register', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "pseudo": pseudo,
+            "email": email,
+            "password": password
+        })
+    })
+    return request.json() 
+}
--- pages/js/script.js
+++ pages/js/script.js
@@ -1,22 +1,24 @@
-var x = document.getElementById("login");
-var y = document.getElementById("register");
-var z = document.getElementById("btn");
+//import { login } from './requests'
 
-function register(){
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+let z = document.getElementById("btn");
+
+function bregister(){
     x.style.left = "-400px";
     y.style.left = "20px";
     z.style.left = "110px";
 }
 
-function login(){
+function blogin(){
     x.style.left = "20px";
     y.style.left = "340px";
     z.style.left = "0px";
 }
 
-var imgHideLogin = document.getElementById("hide-pass-login")
-var imgShowLogin = document.getElementById("show-pass-login")
-var inputPassLogin = document.getElementById("pass-login")
+let imgHideLogin = document.getElementById("hide-pass-login")
+let imgShowLogin = document.getElementById("show-pass-login")
+let inputPassLogin = document.getElementById("pass-login")
 
 function showPassLogin(){
     inputPassLogin.type = "text"
@@ -31,9 +33,9 @@ function hidePassLogin(){
     imgHideLogin.style.display = "block";
 }
 
-var imgHideRegister = document.getElementById("hide-pass-register")
-var imgShowRegister = document.getElementById("show-pass-register")
-var inputPassRegister = document.getElementById("pass-register")
+let imgHideRegister = document.getElementById("hide-pass-register")
+let imgShowRegister = document.getElementById("show-pass-register")
+let inputPassRegister = document.getElementById("pass-register")
 
 function showPassRegister(){
     inputPassRegister.type = "text"
@@ -48,41 +50,88 @@ function hidePassRegister(){
     imgHideRegister.style.display = "block";
 }
 
-var divFogotPassBtn = document.getElementById("forgot-pass")
-var divFogotPass = document.getElementById("forgotpass")
+// Get the modal
+let modal = document.getElementById("all-page");
 
-var loginpage = document.getElementById("login-page")
+// Get the button that opens the modal
+let btn = document.getElementById("client-area");
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+let currentPage = "login-register"
+
+// Element Login/Register Page
+let loginpage = document.getElementById("loginpage")
+let close1 = document.getElementById("close1");
+let divFogotPassBtn = document.getElementById("forgot-pass")
+
+let loginBtn = document.getElementById("submit0")
+let registerBtn = document.getElementById("submit1")
+
+
+
+registerBtn.onclick = function() {
+    //Register Request
+
+}
+
+// Element Verif Page 2
+let pageverif = document.getElementById("pageverif")
+
+
+// Element ForgotPass Page 3
+let divFogotPass = document.getElementById("forgotpass")
+
+
+
+let close4 = document.getElementById("close4");  //Newpassword Page
+
+
+//Function Page 1
+close1.onclick = function() {
+    modal.style.display = "none";
+}
 
 divFogotPassBtn.onclick = function() {
     loginpage.style.display = "none"
     divFogotPass.style.display = "block"
 }
 
-
-
-
-// Get the modal
-var modal = document.getElementById("all-page");
-
-// Get the button that opens the modal
-var btn = document.getElementById("client-area");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-modal.style.display = "block";
+//Function Page 3
+function close2() {
+    pageverif.style.display = "none"  //Close subPage min
+    loginpage.style.display = "block"   //Open subPage main
+    modal.style.display = "none";   //Close subPage
 }
+
+
+//Function Page 3
+function close3() {
+    divFogotPass.style.display = "none"  //Close subPage min
+    loginpage.style.display = "block"   //Open subPage main
+    modal.style.display = "none";   //Close subPage
+}
+
+function submit3Click(){
+    divFogotPass.style.display = "none"
+    pageverif.style.display = "block"
+}
+
+
+close4.onclick = function() {
+    modal.style.display = "none";
+}
+
+
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-modal.style.display = "none";
-}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
-    modal.style.display = "none";
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
